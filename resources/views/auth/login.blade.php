6719b8836e0f12956d4e154d5b5f878b8160e31d
masumotoatuya <atuya9410@gmail.com>
2021-08-30 11:11:38 +0900

    スタート画面のレイアウト

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,18 +4,18 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+            <div >
+                <h2 class="text-center">ログイン</h2>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">メールアドレス</label>
+                            <label for="email"  class="col-md-4 col-form-label text-md-right">メールアドレス</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="test@test.com">
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -29,7 +29,7 @@
                             <label for="password" class="col-md-4 col-form-label text-md-right">パスワード</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                <input  id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="8桁以上の英数字で入力してください">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -39,29 +39,23 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                    ログイン情報を保存する
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
 
                         <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    ログイン
-                                </button>
-
+                            <div class="col-md-8 offset-md-4 text-center">
                                 @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
                                     パスワードを忘れましたか？
                                     </a>
                                 @endif
+                            </div>
+                        </div>
+
+
+                        <div class="form-group row fixed-bottom mb-5">
+                            <div class="col-md-6 offset-md-4 text-center">
+                                <button type="submit" class="btn-lg btn col-8 py-3 btn-primary">
+                                   ログイン
+                                </button>
                             </div>
                         </div>
                     </form>
--- resources/views/start.blade.php
+++ resources/views/start.blade.php
@@ -16,9 +16,19 @@
     <h1>アイコン</h1>
     <h2>アプリ名</h2>
 
+    <div class=" fixed-bottom">
+      <div class="col-md-6 text-center mb-5">
+        <button type="submit" class="btn-lg btn col-8 py-3 btn-primary">
+          新規会員登録
+        </button>
+      </div>
 
-    <button type="button" class="btn btn-primary btn-lg mt-5">Large button</button>
-    <button type="button" class="btn btn-primary btn-lg my-3">Large button</button>
+      <div class="col-md-6  text-center my-5">
+        <button type="submit" class="btn-lg btn col-8 py-3 btn-primary">
+          ログイン
+        </button>
+      </div>
+    </div>
   </div>
 </body>
 
