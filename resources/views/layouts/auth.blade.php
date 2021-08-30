8858a90619088621970130344981ee1764539066
masumotoatuya <atuya9410@gmail.com>
2021-08-30 11:14:41 +0900

    authのナビバーをレスポンシブで非表示

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -21,7 +21,7 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm d-none d-sm-block">
             <div class="container">
                 <a class="navbar-brand d-block d-sm-none" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
