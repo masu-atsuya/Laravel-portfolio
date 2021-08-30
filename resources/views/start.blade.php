3f6b1885c8b70684c559be904387bfada18fe1af
masumotoatuya <atuya9410@gmail.com>
2021-08-30 10:40:24 +0900

    最初の画面のレイアウト

--- resources/views/start.blade.php
+++ resources/views/start.blade.php
@@ -1,6 +1,26 @@
-@extends('layouts.app')
+@extends('layouts.auth')
 
 @section('content')
-最初の画面だよ
-ゲームのマッチングできるよ
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <div class="d-flex align-items-center flex-column my-5">
+    <h1>アイコン</h1>
+    <h2>アプリ名</h2>
+
+
+    <button type="button" class="btn btn-primary btn-lg mt-5">Large button</button>
+    <button type="button" class="btn btn-primary btn-lg my-3">Large button</button>
+  </div>
+</body>
+
+</html>
 @endsection
